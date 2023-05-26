e9de27caf0a9b11a4bcaed5055e25c2a63defbcc
yenye0630 <game02555@gmail.com>
2023-05-26 22:23:21 +0800

    change

---
+++ final/algorithm/dataProcess 2.py
@@ -0,0 +1,140 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA # only for reduce dimension
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import normalize, StandardScaler
+import math
+
+def loadFile(path):
+    train_data = np.genfromtxt(path + 'train_data.csv', delimiter=',')
+    test_data = np.genfromtxt(path + 'test_data.csv', delimiter=',')
+    train_label = np.genfromtxt(path + 'train_label.csv', delimiter=',')
+    test_label = np.genfromtxt(path + 'test_label.csv', delimiter=',')
+    return [train_data, test_data, train_label, test_label]
+
+def dataClean(data):
+    # missing value
+    ratio = 0.3
+    train_missing = np.where(np.isnan(data[0]))
+    missing_index = np.where(np.bincount(train_missing[1])>=data[0].shape[0]*ratio)[0]
+    for i in reversed(missing_index):
+        for j in range(2):
+            data[j] = np.delete(data[j], i, axis=1)
+    imp_mean = SimpleImputer(missing_values=np.NaN, strategy='median')
+    for i in range(2):
+        imputer = imp_mean.fit(data[i])
+        data[i] = imputer.transform(data[i])
+        std_scaler = StandardScaler()
+        data[i] = std_scaler.fit_transform(data[i]) 
+
+    # outlier value
+    for i in reversed([0, 2, 3]): #age, Height, Weight
+        for j in range(2):
+            outlier = np.where(data[j][:, i] == 0.0)[0]
+            outlier = np.concatenate((outlier, np.where(data[j][:, i] >= 250.0)[0]))
+            data[j] = np.delete(data[j], outlier, axis=0)
+            data[j+2] = np.delete(data[j+2], outlier, axis=0)
+    return data
+
+def excutePCA(data, showPCA=False):
+    # Implementation of PCA
+    pca_dict = {}
+    eigen_dict = {}
+    for n_comp in range(data[0].shape[1]):
+        pca = PCA(n_components=n_comp)
+        temp_train_pca = pca.fit_transform(data[0])
+        temp_test_pca = pca.transform(data[1])
+        eigen_values = pca.explained_variance_[:n_comp]
+        if n_comp > 0:
+            pca_dict[n_comp] = pca.explained_variance_ratio_.sum()
+            eigen_dict[n_comp] = eigen_values[-1]
+    if showPCA:
+        f = plt.figure(1)
+        f.patch.set_facecolor('white')
+        plt.title('PCA Variance')
+        plt.xlabel('Principal Component Number')
+        plt.ylabel('Variance Ratio')
+        plt.plot(list(pca_dict.keys()),list(pca_dict.values()),'r')
+        f.show()
+
+        g = plt.figure(2)
+        g.patch.set_facecolor('white')
+        plt.title('PCA Eigen value')
+        plt.xlabel('Principal Component Number')
+        plt.ylabel('Eigen Values')
+        plt.plot(list(eigen_dict.keys()),list(eigen_dict.values()),'r')
+        g.show()
+        g.savefig('pca.png')
+    # Selecting components with Eigen value greater than 1 from the list
+    pca_comp_eigen = max([key for key,val in eigen_dict.items() if val >= 1])
+    pca_comp_eigen = max([key for key,val in pca_dict.items() if val < 0.95])
+
+    print('Components from Feature selection using PCA (Having Eigen values >=1)- ' + str(pca_comp_eigen) + '')
+    # Performing PCA for the train data with the fixed components
+    pca = PCA(n_components=pca_comp_eigen)
+    data[0] = pca.fit_transform(data[0])
+    data[1] = pca.transform(data[1])
+    print('Feature Selection using PCA complete for the train data.')
+    return data
+
+
+def dataProcessing(path, clean_data, PCAMethod, norm_data=None):
+    print('===========================================================================')
+    print('Loading datasets...')
+    data = loadFile(path)
+    org_train = data[0].shape
+    org_test = data[1].shape
+    print(f'The original training data have {org_train}.')
+    print(f'The original testing  data have {org_test}.')
+    if clean_data == 'is_clean':
+        data = dataClean(data)
+        print('\nCleaning data...')
+        print(f'clean original training data: {org_train} -> {data[0].shape}')
+        print(f'clean original testing  data: {org_test} -> {data[1].shape}')
+    org_train = data[0].shape
+    org_test = data[1].shape
+    if PCAMethod == 'using_pca':
+        data = excutePCA(data)
+        print('\nPCA...')
+        print(f'original training data: {org_train} -> {data[0].shape}')
+        print(f'original testing  data: {org_test} -> {data[1].shape}')
+    if norm_data in 'Min-Max':
+        data = norm(data)
+        print('\nMin-Max normalization method...')
+    elif norm_data == 'Z-score':
+        data = standard(data)
+        print('\nZ-score normalization method...')
+    print('===========================================================================')
+    train_data = data[0]
+    test_data = data[1]
+    train_label = data[2]
+    test_label = data[3]
+    return train_data, test_data, train_label, test_label
+
+def norm(data):
+    for i in range(data[0].shape[1]):
+        data[0][:,i] = (data[0][:,i] - np.min(data[0][:,i]))/(np.max(data[0][:,i]) - np.min(data[0][:,i]))
+        data[1][:,i] = (data[1][:,i] - np.min(data[1][:,i]))/(np.max(data[1][:,i]) - np.min(data[1][:,i]))
+    return data
+
+def standard(data):
+    train_mu = np.mean(data[0], axis=0)
+    train_std = np.std(data[0], axis=0)
+    test_mu = np.mean(data[1], axis=0)
+    test_std = np.std(data[1], axis=0)
+    data[0] = (data[0] - train_mu)/train_std
+    data[1] = (data[1] - test_mu)/test_std
+    return data
+
+def main():
+    path = 'Arrhythmia Data Set/'
+    clean = True
+    pca = True
+    train_data, test_data, train_label, test_label =\
+        dataProcessing(path, clean_data=clean, PCAMethod=pca, norm_data='Z-score')
+
+    print(train_data.shape)
+    print(train_label)
+
+if __name__ == "__main__":
+    data = main()
---
+++ final/algorithm/data_mining_final.py
@@ -0,0 +1,253 @@
+import torch
+import numpy as np
+from torch.utils.data import DataLoader
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import MinMaxScaler
+from torch import nn
+from torch import optim
+import torch.optim.lr_scheduler as lr_scheduler
+from dataProcess import *
+import copy
+from collections import Counter
+from imblearn.over_sampling import SMOTE
+import itertools
+
+class FC(nn.Module):
+	"""Fully connected neural network"""
+	def __init__(self, input_size, hidden_size, output_size):
+		super(FC, self).__init__()
+		self.layer = nn.ParameterList([
+			nn.Linear(input_size, hidden_size),
+			nn.Linear(hidden_size, output_size)
+		])
+		self.activation = nn.LeakyReLU()
+
+	def forward(self, x):
+		for i in range(len(self.layer) - 1):
+			x = self.layer[i](x)
+			x = self.activation(x)
+
+		x = self.layer[-1](x)
+		return x
+
+def using_smote(train_data, train_label):
+	# Extract the eighth data from dataset first as we cannot apply SMOTE on a class that contains only one data point
+    # We'll duplicate this data to the same size as the data in other classes
+	eighth_data = train_data[0]
+	eighth_label = train_label[0]
+	train_data = train_data[1:]
+	train_label = train_label[1:]
+
+	# Minus one from every label because when using crossentropy loss the starting label is 0
+	train_label = [y - 1 for y in train_label]
+
+	sm = SMOTE(random_state=42, k_neighbors=2)
+	X_res, y_res = sm.fit_resample(train_data, train_label)
+
+	# Count the max number of elements in one class for duplicating eighth class element later
+	counter = Counter(y_res)
+	sorted_counts = counter.most_common()
+	max_count = sorted_counts[0][1]
+
+	X_res = torch.cat((torch.Tensor(X_res), torch.Tensor([eighth_data for _ in range(max_count)])))
+	y_res = torch.cat((torch.Tensor(y_res), torch.Tensor([eighth_label - 1 for _ in range(max_count)]))).type(torch.LongTensor)
+
+	return X_res, y_res
+
+def without_smote(train_data, train_label):
+	# Minus one from every label because when using crossentropy loss the starting label is 0
+	train_label = [y - 1 for y in train_label]
+	X = torch.Tensor(train_data)
+	y = torch.Tensor(train_label).type(torch.LongTensor)
+
+	return X, y
+
+def predict(model, dataset):
+	'''
+	return: a dict of predictions with key being the prediction and value being the true label
+	(every prediction contains 8 different values representing the predicted probabilities of each class)
+	'''
+	with torch.no_grad():
+		acc = []
+		pred = []
+		total_samples = 0
+		prob_list = {}
+		for inputs, labels in dataset:
+			outputs = model(inputs)
+			_, predicted = torch.max(outputs, dim=0)
+			pred.append(predicted.item())
+			predicted_probabilities = torch.softmax(outputs, dim=0)
+			prob_list[tuple(predicted_probabilities.tolist())] = labels
+			if predicted == labels:
+				acc.append(1)
+			else:
+				acc.append(0)
+
+			total_samples += len(inputs)
+
+		pred_acc = np.array(acc).mean()
+		print(f'Accuracy: {pred_acc}')
+
+	return prob_list
+
+def main():
+
+	dataset_path = 'Arrhythmia Data Set/'
+	clean = 'is_clean'
+	pca = ['using_pca', 'without_pca']
+	smote = ['using_smote', 'without_smote']
+	norm_data = ['None']
+	is_inference = False
+
+	# Get all combinations of options
+	all_options = itertools.product(pca, smote, norm_data)
+
+	if not is_inference:
+		# Iterate through all the combinations
+		for option in all_options:
+			pca, smote, norm_data = option
+			print('--------------------Options--------------------')
+			print(f'Clean: {clean}')
+			print(f'PCA: {pca}')
+			print(f'SMOTE: {smote}')
+			print(f'Normalization: {norm_data}')
+
+			train_data, _, train_label, _ =\
+				dataProcessing(dataset_path, clean_data=clean, PCAMethod=pca, norm_data=norm_data)
+
+			if smote == 'using_smote':
+				X, y = using_smote(train_data, train_label)
+				X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4, stratify=y)
+			else:
+				X, y = without_smote(train_data, train_label)
+				X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=4)
+
+			train_dataset = torch.utils.data.TensorDataset(X_train, y_train)
+			test_dataset = torch.utils.data.TensorDataset(X_test, y_test)
+
+			# Hyperparameters
+			batch_size = 128
+			shuffle_data = True
+			input_size = train_data.shape[1]
+			hidden_size = 128
+			output_size = 8
+			num_epochs = 100
+
+			model = FC(input_size, hidden_size, output_size)
+			criterion = nn.CrossEntropyLoss()
+			optimizer = optim.Adam(model.parameters(), lr=1e-2, weight_decay=1e-2)
+			scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, factor=1/3)
+
+			max_acc = 0
+			lowest_val_loss = 999999
+
+			data_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=shuffle_data)
+			model_save = None
+
+			# Training
+			for epoch in range(num_epochs):
+			    model.train()
+				# Iterate over the data_loader for training
+			    for batch in data_loader:
+			        inputs, labels = batch
+
+			        # Forward pass
+			        outputs = model(inputs)
+
+			        # Compute the loss
+			        loss = criterion(outputs, labels)
+
+			        # Backward pass and optimization
+			        optimizer.zero_grad()
+			        loss.backward()
+			        optimizer.step()
+
+			    # Print the loss after each epoch
+			    #print(f"Epoch {epoch + 1} / {num_epochs}, Loss: {loss.item():.4f}")
+
+			    model.eval()
+			    with torch.no_grad():
+			    	acc = []
+			    	val_loss = 0
+			    	total_samples = 0
+			    	for inputs, labels in test_dataset:
+			    		outputs = model(inputs)
+			    		_, predicted = torch.max(outputs, dim=0)
+			    		if predicted == labels:
+			    			acc.append(1)
+			    		else:
+			    			acc.append(0)
+
+			    		loss = criterion(outputs, labels)
+			    		val_loss += loss.item() * len(inputs)
+			    		total_samples += len(inputs)
+
+			    	pred_acc = np.array(acc).mean()
+			    	if pred_acc > max_acc:
+			    		max_acc = pred_acc
+			    		max_acc_epoch = epoch + 1
+			    	#print(f'val acc: {pred_acc}')
+
+			    	val_loss /= total_samples
+			    	if lowest_val_loss > val_loss:
+			    		lowest_val_loss = val_loss
+			    		lowest_val_loss_epoch = epoch + 1
+			    		# Save the checkpoint that has the lowest validation loss
+			    		model_save = copy.deepcopy(model)
+
+			    	#print(f'val loss: {val_loss:.4f}')
+
+			    scheduler.step(val_loss)
+
+			print(f'Max acc: {max_acc} at {max_acc_epoch} epoch')
+			print(f'Lowest vaidation loss: {lowest_val_loss} at {lowest_val_loss_epoch} epoch')
+
+			PATH = f'model_{clean}_{pca}_{smote}_norm_option_{norm_data}.pt'
+			# Save the best model to PATH
+			torch.save(model_save.state_dict(), PATH)
+
+			with torch.no_grad():
+				acc = []
+				pred = []
+				val_loss = 0
+				total_samples = 0
+				for inputs, labels in test_dataset:
+					outputs = model(inputs)
+					_, predicted = torch.max(outputs, dim=0)
+					pred.append(predicted.item())
+					if predicted == labels:
+						acc.append(1)
+					else:
+						acc.append(0)
+
+					val_loss += loss.item() * len(inputs)
+					total_samples += len(inputs)
+
+				pred_acc = np.array(acc).mean()
+				print(f'final acc: {pred_acc}')
+
+	else:
+		# Preparing data, using the above test set as example
+		train_data, _, train_label, _ =\
+			dataProcessing(dataset_path, clean_data=clean, PCAMethod='without_pca', norm_data='None')
+		X, y = using_smote(train_data, train_label)
+		_, X_test, _, y_test = train_test_split(X, y, test_size=0.2, random_state=4, stratify=y)
+		test_dataset = torch.utils.data.TensorDataset(X_test, y_test)
+		input_size = X_test.shape[1]
+		hidden_size = 128
+		output_size = 8
+
+		# Inference
+		model_path = 'model_is_clean_without_pca_using_smote_norm_option_None.pt'
+		model = FC(input_size, hidden_size, output_size)
+		model.load_state_dict(torch.load(model_path))
+		predicted_probabilities = predict(model, test_dataset)
+
+		for pred, label in predicted_probabilities.items():
+			if np.argmax(pred, axis=0) == label:
+				print(f'{label}: {["%0.2f" % i for i in pred]} right prediction')
+			else:
+				print(f'{label}: {["%0.2f" % i for i in pred]} wrong prediction')
+
+if __name__ == "__main__":
+	main()
